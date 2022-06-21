867f949b63c0b04c511f5c4ce9f2691e9e551937
shahman-riaz <shahmanriaz07@gmail.com>
2022-06-22 00:50:45 +0600

    updating

---
+++ buildin_module.py
@@ -0,0 +1,18 @@
+# import time
+
+# while True:
+#   localtime = time.localtime()
+#   result = time.strftime("%I:%M:%S %p", localtime)
+#   print(result)
+#   time.sleep(1)
+
+
+
+
+import time
+
+while True:
+    localtime = time.localtime()
+    result = time.starft("%I : %M : %S %p", localtime)
+    print(result)
+    time.s
--- write_file.py
+++ write_file.py
@@ -1,2 +1,5 @@
-with open("vegetables.txt", "w") as myfile:
-    content = myfile.write('Tomato\nCucumber\nOnion\nChilli')
+with open("bear.txt", "r") as read_file:
+    content1 = read_file.read()[0:90]
+
+with open("first.txt", 'w') as write_file:
+    content2 = write_file.write(content1)
