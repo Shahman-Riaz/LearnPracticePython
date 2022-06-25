b6f80073edc3f51234f854da316e1874e9e8a7b9
shahman-riaz <shahmanriaz07@gmail.com>
2022-06-25 18:32:37 +0600

    solved one

--- Geeksforgeeks/problem7.py
+++ Geeksforgeeks/problem7.py
@@ -1,21 +1,28 @@
+## *Convert a sentence into its equivalent mobile numeric keypad sequence *
 x = input("")
+y = x.lower()
 num = ''
-for i in x:
+for i in y:
+    if i in ' ':
+        num += '0'
     if i in 'abc':
-        num += '2'
-    elif i in 'def':
-        num += '3'
-    elif i in 'ghi':
-        num += '4'
-    elif i in 'jkl':
-        num += '5'
-    elif i in 'mno':
-        num += '6'
-    elif i in 'pqrs':
-        num += '7'
-    elif i in 'tuv':
-        num += '8'
-    elif i in 'wxyz':
-        num += '9'
-print(int(num))
+        num += '2' if i == 'a'  else '22' if i == 'b' else '222'
+    if i in 'def':
+        num += '3' if i == 'd'  else '33' if i == 'e' else '333'
+    if i in 'ghi':
+        num += '4' if i == 'g'  else '44' if i == 'h' else '444'
+    if i in 'jkl':
+        num += '5' if i == 'j'  else '55' if i == 'k' else '555'
+    if i in 'mno':
+        num += '6' if i == 'm'  else '66' if i == 'n' else '666'
+    if i in 'pqrs':
+        num += '7' if i == 'p'  else '77' if i == 'q' else '777' if i == 'r' else '7777'
+    if i in 'tuv':
+        num += '8' if i == 't'  else '88' if i == 'u' else '888'
+    if i in 'wxyz':
+        num += '9' if i == 'w'  else '99' if i == 'x' else '999' if i == 'y' else '9999'
     
+print((num))
+    
+
+#solved
---
+++ Geeksforgeeks/string.py
@@ -0,0 +1,6 @@
+gfg = '793a'
+# print(gfg[::-1])
+gfg2 = ''.join(reversed(gfg))
+# print(gfg2)
+
+print(gfg.isdecimal())
